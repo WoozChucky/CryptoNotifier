0a788d553c61f2c76b23d088cd07d17dc214eefd
Nuno Silva <nuno.levezinho@live.com.pt>
2017-11-30 21:56:58 +0000

    Created default projects

---
+++ CryptoNotifier.Common/CoinbaseAPI.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+
+namespace CryptoNotifier.Common
+{
+    public class CoinbaseAPI
+    {
+        public const string DEFAULT_BASE_URL = "https://api.coinbase.com/v2/";
+
+        private string _api_key;
+        private string _api_secret;
+        private readonly string _base_url;
+        private readonly RestClient _client;
+
+        public CoinbaseAPI(string api_url = DEFAULT_BASE_URL)
+        {
+
+        }
+
+        public void Provide(string key, string secret)
+        {
+            _api_key = key;
+            _api_secret = secret;
+        }
+    }
+}
---
+++ CryptoNotifier.Core/Main.cs
@@ -0,0 +1,15 @@
+﻿using CryptoNotifier.Common;
+using System;
+
+namespace CryptoNotifier.Core
+{
+    public class Main
+    {
+        
+        public void Execute()
+        {
+            
+        }
+
+    }
+}
